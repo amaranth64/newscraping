b497037fe6c52878ee8c617dc63c1c06984a03a3
amaranth64 <https://github.com/amaranth64/scraping>
2021-02-13 15:46:12 +0400

    heroku2

--- scraping_service/settings/production.py
+++ scraping_service/settings/production.py
@@ -36,7 +36,7 @@ SECRET_KEY = SECRET_KEY
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["serv-scrap.herokuapp.com"]
+ALLOWED_HOSTS = ["myscrap-serv.herokuapp.com"]
 
 
 # Application definition
