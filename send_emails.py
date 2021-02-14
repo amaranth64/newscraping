bd78cb8f79f02cdd1a85697b433e675574a66b33
amaranth64 <https://github.com/amaranth64/scraping>
2021-02-14 16:38:25 +0400

    исправлена рассылка

--- send_emails.py
+++ send_emails.py
@@ -94,22 +94,3 @@ if subject:
     msg = EmailMultiAlternatives(subject, text_content, from_email, [to])
     msg.attach_alternative(_html, "text/html")
     msg.send()
-
-# import smtplib
-# from email.mime.multipart import MIMEMultipart
-# from email.mime.text import MIMEText
-#
-# msg = MIMEMultipart('alternative')
-# msg['Subject'] = 'Список вакансий за  {}'.format(today)
-# msg['From'] = EMAIL_HOST_USER
-# mail = smtplib.SMTP()
-# mail.connect(EMAIL_HOST, 25)
-# mail.ehlo()
-# mail.starttls()
-# mail.login(EMAIL_HOST_USER, EMAIL_HOST_PASSWORD)
-#
-# html_m = "<h1>Hello world</h1>"
-# part = MIMEText(html_m, 'html')
-# msg.attach(part)
-# mail.sendmail(EMAIL_HOST_USER, [to], msg.as_string())
-# mail.quit()
